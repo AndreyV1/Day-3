b9f1b814050cc6f2852d46ec92ccb9393749bd4c
Andrey <andrey2004182@gmail.com>
2023-05-30 18:41:53 +0300

    Добавьте файлы проекта.

---
+++ Day-3/Employee.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_3
+{
+    class Employee
+    {
+        private static int idCounter = 1;
+        private int id;
+        private string fullName;
+        private int department;
+        private double salary;
+
+        public Employee(string fullName, int department, double salary)
+        {
+            this.id = idCounter++;
+            this.fullName = fullName;
+            this.department = department;
+            this.salary = salary;
+        }
+
+        public int Id
+        {
+            get { return id; }
+        }
+
+        public string FullName
+        {
+            get { return fullName; }
+        }
+
+        public int Department
+        {
+            get { return department; }
+            set { department = value; }
+        }
+
+        public double Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+
+        public static void PrintAllEmployees(Employee[] employees)
+        {
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine("ID: {0}, ФИО: {1}, Отдел: {2}, Зарплата: {3}", employee.Id, employee.FullName, employee.Department, employee.Salary);
+            }
+        }
+
+        public static double CalculateTotalSalary(Employee[] employees)
+        {
+            double totalSalary = 0;
+            foreach (Employee employee in employees)
+            {
+                totalSalary += employee.Salary;
+            }
+            return totalSalary;
+        }
+
+        public static Employee FindEmployeeWithMinSalary(Employee[] employees)
+        {
+            Employee minSalaryEmployee = employees[0];
+            foreach (Employee employee in employees)
+            {
+                if (employee.Salary < minSalaryEmployee.Salary)
+                {
+                    minSalaryEmployee = employee;
+                }
+            }
+            return minSalaryEmployee;
+        }
+
+        public static Employee FindEmployeeWithMaxSalary(Employee[] employees)
+        {
+            Employee maxSalaryEmployee = employees[0];
+            foreach (Employee employee in employees)
+            {
+                if (employee.Salary > maxSalaryEmployee.Salary)
+                {
+                    maxSalaryEmployee = employee;
+                }
+            }
+            return maxSalaryEmployee;
+        }
+
+        public static double CalculateAverageSalary(Employee[] employees)
+        {
+            double totalSalary = CalculateTotalSalary(employees);
+            return totalSalary / employees.Length;
+        }
+    }
+}
---
+++ Day-3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[10];
+            employees[0] = new Employee("Иванов Иван Иванович", 1, 25000);
+            employees[1] = new Employee("Петров Петр Петрович", 2, 30000);
+            employees[2] = new Employee("Сидорова Анна Ивановна", 3, 52000);
+            employees[3] = new Employee("Кузнецова Елена Александровна", 4, 47000);
+            employees[4] = new Employee("Смирнов Алексей Владимирович", 5, 70000);
+            employees[5] = new Employee("Новиков Дмитрий Сергеевич", 1, 40000);
+            employees[6] = new Employee("Морозова Ольга Викторовна", 2, 50000);
+            employees[7] = new Employee("Козлов Андрей Игоревич", 3, 55000);
+            employees[8] = new Employee("Лебедева Наталья Александровна", 4, 60000);
+            employees[9] = new Employee("Григорьев Сергей Васильевич", 5, 75000);
+
+            Employee.PrintAllEmployees(employees);
+
+            double totalSalary = Employee.CalculateTotalSalary(employees);
+            Console.WriteLine("Общая зарплата: {0}", totalSalary);
+
+            Employee minSalaryEmployee = Employee.FindEmployeeWithMinSalary(employees);
+            Console.WriteLine("Сотрудник с минимальной зарплатой: {0}", minSalaryEmployee.FullName);
+
+            Employee maxSalaryEmployee = Employee.FindEmployeeWithMaxSalary(employees);
+            Console.WriteLine("Сотрудник с максимальной зарплатой: {0}", maxSalaryEmployee.FullName);
+
+            double averageSalary = Employee.CalculateAverageSalary(employees);
+            Console.WriteLine("Средняя зарплата: {0}", averageSalary);
+        }
+    }
+}
