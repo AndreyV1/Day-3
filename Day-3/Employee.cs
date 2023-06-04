d883318a70ac2d1d22199accc8ac0cb30804121d
Andrey <andrey2004182@gmail.com>
2023-06-04 14:40:28 +0300

    Complete

--- Day-3/Employee.cs
+++ Day-3/Employee.cs
@@ -6,92 +6,43 @@ using System.Threading.Tasks;
 
 namespace Day_3
 {
-    class Employee
+    internal class Employee
     {
-        private static int idCounter = 1;
-        private int id;
-        private string fullName;
-        private int department;
-        private double salary;
+        private string _fio;
+        private double _salary;
+        private int _department;
+        private int _id = 0;
+        static int count;
 
-        public Employee(string fullName, int department, double salary)
+        public int Id()
         {
-            this.id = idCounter++;
-            this.fullName = fullName;
-            this.department = department;
-            this.salary = salary;
+            _id++;
+            count = _id;
+            return count;
         }
 
-        public int Id
+        public Employee(string fio, double salary, int department)
         {
-            get { return id; }
+
+            _fio = fio;
+            _salary = salary;
+            _department = department;
         }
 
-        public string FullName
+        public string Fio
         {
-            get { return fullName; }
+            get => _fio;
+            set => _fio = value;
         }
-
-        public int Department
-        {
-            get { return department; }
-            set { department = value; }
-        }
-
         public double Salary
         {
-            get { return salary; }
-            set { salary = value; }
+            get => _salary;
+            set => _salary = value;
         }
-
-        public static void PrintAllEmployees(Employee[] employees)
+        public int Department
         {
-            foreach (Employee employee in employees)
-            {
-                Console.WriteLine("ID: {0}, ФИО: {1}, Отдел: {2}, Зарплата: {3}", employee.Id, employee.FullName, employee.Department, employee.Salary);
-            }
-        }
-
-        public static double CalculateTotalSalary(Employee[] employees)
-        {
-            double totalSalary = 0;
-            foreach (Employee employee in employees)
-            {
-                totalSalary += employee.Salary;
-            }
-            return totalSalary;
-        }
-
-        public static Employee FindEmployeeWithMinSalary(Employee[] employees)
-        {
-            Employee minSalaryEmployee = employees[0];
-            foreach (Employee employee in employees)
-            {
-                if (employee.Salary < minSalaryEmployee.Salary)
-                {
-                    minSalaryEmployee = employee;
-                }
-            }
-            return minSalaryEmployee;
-        }
-
-        public static Employee FindEmployeeWithMaxSalary(Employee[] employees)
-        {
-            Employee maxSalaryEmployee = employees[0];
-            foreach (Employee employee in employees)
-            {
-                if (employee.Salary > maxSalaryEmployee.Salary)
-                {
-                    maxSalaryEmployee = employee;
-                }
-            }
-            return maxSalaryEmployee;
-        }
-
-        public static double CalculateAverageSalary(Employee[] employees)
-        {
-            double totalSalary = CalculateTotalSalary(employees);
-            return totalSalary / employees.Length;
+            get => _department;
+            set => _department = value;
         }
     }
 }
--- Day-3/Program.cs
+++ Day-3/Program.cs
@@ -1,40 +1,114 @@
-﻿using System;
+﻿using Day_3;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Day_3
+internal class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Employee employee = new Employee("fio", 10000, 1);
+        List<Employee> employees = new List<Employee>()
         {
-            Employee[] employees = new Employee[10];
-            employees[0] = new Employee("Иванов Иван Иванович", 1, 25000);
-            employees[1] = new Employee("Петров Петр Петрович", 2, 30000);
-            employees[2] = new Employee("Сидорова Анна Ивановна", 3, 52000);
-            employees[3] = new Employee("Кузнецова Елена Александровна", 4, 47000);
-            employees[4] = new Employee("Смирнов Алексей Владимирович", 5, 70000);
-            employees[5] = new Employee("Новиков Дмитрий Сергеевич", 1, 40000);
-            employees[6] = new Employee("Морозова Ольга Викторовна", 2, 50000);
-            employees[7] = new Employee("Козлов Андрей Игоревич", 3, 55000);
-            employees[8] = new Employee("Лебедева Наталья Александровна", 4, 60000);
-            employees[9] = new Employee("Григорьев Сергей Васильевич", 5, 75000);
+                new Employee("Иванов Иван Иванович", 25000, 1),
+                new Employee("Петров Петр Петрович", 30200, 2),
+                new Employee("Сидорова Анна Ивановна", 52000, 3),
+                new Employee("Кузнецова Елена Александровна", 47000, 4),
+                new Employee("Смирнов Алексей Владимирович", 70000, 5),
+                new Employee("Новиков Дмитрий Сергеевич", 40700, 1),
+                new Employee("Морозова Ольга Викторовна", 50000, 2),
+                new Employee("Козлов Андрей Игоревич", 55000, 3),
+                new Employee("Лебедева Наталья Александровна", 60500, 4),
+                new Employee("Григорьев Сергей Васильевич", 65000, 5)
 
-            Employee.PrintAllEmployees(employees);
+        };
 
-            double totalSalary = Employee.CalculateTotalSalary(employees);
-            Console.WriteLine("Общая зарплата: {0}", totalSalary);
+        void Employee()
+        {
+            Console.WriteLine("Employee");
+            foreach (var emp in employees)
+            {
 
-            Employee minSalaryEmployee = Employee.FindEmployeeWithMinSalary(employees);
-            Console.WriteLine("Сотрудник с минимальной зарплатой: {0}", minSalaryEmployee.FullName);
-
-            Employee maxSalaryEmployee = Employee.FindEmployeeWithMaxSalary(employees);
-            Console.WriteLine("Сотрудник с максимальной зарплатой: {0}", maxSalaryEmployee.FullName);
-
-            double averageSalary = Employee.CalculateAverageSalary(employees);
-            Console.WriteLine("Средняя зарплата: {0}", averageSalary);
+                Console.WriteLine($"Id: {employee.Id()} ФИО: {emp.Fio}, Зарплата: {emp.Salary}, Отдел: {emp.Department}");
+            }
         }
+
+        void TotalSalary()
+        {
+            Console.WriteLine("TotalSalary");
+            double summa = 0;
+            foreach (var emp in employees)
+            {
+                summa += emp.Salary;
+            }
+            Console.WriteLine($"Сумма затрат на зарплату в месяц: {summa}");
+        }
+
+        void MinSalary()
+        {
+            Console.WriteLine("MinSalary");
+            double min = 99999;
+            foreach (var emp in employees)
+            {
+                if (emp.Salary < min)
+                {
+                    min = emp.Salary;
+                }
+            }
+            Console.WriteLine("Минимальная зарплата " + min);
+        }
+
+        void MaxSalary()
+        {
+            Console.WriteLine("MaxSalary");
+            double max = 0;
+            foreach (var emp in employees)
+            {
+                if (emp.Salary > max)
+                {
+                    max = emp.Salary;
+                }
+            }
+            Console.WriteLine("Максимальная зарплата " + max);
+        }
+
+        void AverageSalary()
+        {
+            Console.WriteLine("AverageSalary");
+            int count = 0;
+            double summa = 0;
+            foreach (var emp in employees)
+            {
+                count++;
+                summa += emp.Salary;
+            }
+
+            Console.WriteLine($"Среднее значение зарплаты: {summa / count}");
+        }
+
+        void FullName()
+        {
+            Console.WriteLine("FullName");
+            foreach (var emp in employees)
+            {
+                Console.WriteLine("ФИО " + emp.Fio);
+            }
+            Console.ReadKey();
+        }
+
+        Employee();
+        Console.WriteLine();
+        TotalSalary();
+        Console.WriteLine();
+        MinSalary();
+        Console.WriteLine();
+        MaxSalary();
+        Console.WriteLine();
+        AverageSalary();
+        Console.WriteLine();
+        FullName();
     }
 }
+
